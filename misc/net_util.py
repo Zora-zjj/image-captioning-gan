454c4167eef7949212e92d497d7eea52091c6005
Zora-zjj <54018191+Zora-zjj@users.noreply.github.com>
2020-04-23 21:57:51 +0800

    Update net_util.py

--- misc/net_util.py
+++ misc/net_util.py
@@ -1,5 +1,5 @@
 def remove_module(state):
     new_state = {}
     for key, val in state.items():
-        new_state[key[key.index(".") + 1:]] = val
+        new_state[key[key.index(".") + 1:]] = val   #str.index(str, beg=0, end=len(string))  检测字符串中是否包含子字符串 str 
     return new_state
