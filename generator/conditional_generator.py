26c9ccd82675437997bdd230d658be48de510c0c
Zora-zjj <54018191+Zora-zjj@users.noreply.github.com>
2020-04-23 13:18:09 +0800

    Update conditional_generator.py

--- generator/conditional_generator.py
+++ generator/conditional_generator.py
@@ -27,7 +27,7 @@ class ConditionalGenerator(nn.Module):
         self.input_encoding_size = input_encoding_size
         self.max_sentence_length = max_sentence_length
         self.embed = corpus
-        self.dist = Normal(Variable(mean), Variable(std))  # noise variable
+        self.dist = Normal(Variable(mean), Variable(std))  # noise variable # torch.distributions.Normal定义正态分布
         self.lstm = nn.LSTM(input_size=corpus.embed_size,
                             hidden_size=self.input_encoding_size,
                             num_layers=num_layers,
@@ -38,13 +38,13 @@ class ConditionalGenerator(nn.Module):
         self.features_linear = nn.Sequential(
             nn.Linear(cnn_output_size + len(mean), input_encoding_size),
             nn.ReLU()
-        )
-        self.rollout = Rollout(max_sentence_length, corpus)
+        )  
+        self.rollout = Rollout(max_sentence_length, corpus)    
 
     def init_hidden(self, image_features):
 
         # generate rand
-        rand = self.dist.sample((image_features.shape[0],)).cuda()
+        rand = self.dist.sample((image_features.shape[0],)).cuda()   #.sample直接在定义的正态分布上采样，此处的sample是后面定义的函数
 
         # hidden of shape (num_layers * num_directions, batch, hidden_size)
         hidden = self.features_linear(torch.cat((image_features, rand), 1).unsqueeze(0))
@@ -61,7 +61,7 @@ class ConditionalGenerator(nn.Module):
         return outputs
 
     def reward_forward(self, image_features, evaluator, monte_carlo_count=16):
-        self.lstm.flatten_parameters()
+        self.lstm.flatten_parameters()      #.flatten_parameters 重置参数的数据指针，让数据存放成连续的块
         batch_size = image_features.size(0)
         hidden = self.init_hidden(image_features)
         # embed the start symbol
@@ -74,8 +74,8 @@ class ConditionalGenerator(nn.Module):
             _, hidden = self.lstm(inputs, hidden)
             outputs = self.output_linear(hidden[0]).squeeze(0)
             outputs = F.softmax(outputs, -1)
-            predicted = outputs.multinomial(1)
-            prop = torch.gather(outputs, 1, predicted)
+            predicted = outputs.multinomial(1)      #.multinomial(input,num_samples) 对每一行做num次取值，返回下标
+            prop = torch.gather(outputs, 1, predicted)     #torch.gather(input,dim,index)在dim维度上对input取index的数据
             props[:, i] = prop.view(-1)
             # embed the next inputs, unsqueeze is required cause of shape (batch_size, 1, embedding_size)
             inputs = self.embed.word_embeddings_from_indices(predicted.view(-1).cpu().data.numpy()).unsqueeze(1).cuda()
@@ -175,7 +175,7 @@ class ConditionalGenerator(nn.Module):
             inputs = Variable(inputs)
             _, hidden = self.lstm(inputs, hidden)
             outputs = self.output_linear(hidden[0]).squeeze(0)
-            predicted = outputs.max(-1)[1]
+            predicted = outputs.max(-1)[1]     #.max(-1)[1]    （-1）表示按照最后一个维度求最大值，[1]表示返回最大值的索引，即返回0或1
 
             # embed the next inputs, unsqueeze is required 'cause of shape (batch_size, 1, embedding_size)
             inputs = self.embed.word_embeddings_from_indices(predicted.cpu().data.numpy()).unsqueeze(1).cuda()
