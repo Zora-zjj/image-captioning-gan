d5f49a59a72dd645c44ec5dbca4a5001eb193cc9
Zora-zjj <54018191+Zora-zjj@users.noreply.github.com>
2020-04-23 22:03:28 +0800

    Update file_path_manager.py

--- file_path_manager.py
+++ file_path_manager.py
@@ -2,8 +2,8 @@ import os
 
 
 class FilePathManager:
-    base_dir = os.path.dirname(os.path.abspath(__file__))
+    base_dir = os.path.dirname(os.path.abspath(__file__))  #os.path.dirname  去掉文件名，返回首目录    os.path.abspath  返回绝对路径
 
-    @staticmethod
+    @staticmethod        # @staticmethod    修饰器，静态方法 
     def resolve(path):
         return f"{FilePathManager.base_dir}/{path}"
