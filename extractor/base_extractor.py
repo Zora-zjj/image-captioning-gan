8c8997ab780e113db743aff94f62d2628baae0ec
Zora-zjj <54018191+Zora-zjj@users.noreply.github.com>
2020-04-23 13:19:54 +0800

    Update base_extractor.py

--- extractor/base_extractor.py
+++ extractor/base_extractor.py
@@ -1,7 +1,7 @@
 from abc import abstractmethod, ABCMeta
 
 
-class BaseExtractor(metaclass=ABCMeta):
+class BaseExtractor(metaclass=ABCMeta):          # metaclass=ABCMeta 多态性指具有不同功能的函数可以使用相同的函数名
     def __init__(self, use_gpu: bool = True):
         self.use_gpu = use_gpu
 
