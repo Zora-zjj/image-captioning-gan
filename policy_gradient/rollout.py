f07a2447d5ce005fdb7c70977a02f45ed2ba93e3
Zora-zjj <54018191+Zora-zjj@users.noreply.github.com>
2020-04-29 21:34:25 +0800

    Update rollout.py

--- policy_gradient/rollout.py
+++ policy_gradient/rollout.py
@@ -20,7 +20,7 @@ class Rollout:
         monte_carlo_count //= steps
 
         with torch.no_grad():
-            batch_size = generated.size(0)
+            batch_size = generated.size(0)    #generated : [batch,句子长度，单词维度]
             result = torch.zeros(batch_size, 1).cuda()
             remaining = self.max_sentence_length - generated.shape[1]
             h, c = hidden
@@ -29,16 +29,16 @@ class Rollout:
                 hidden = (h.repeat(1, monte_carlo_count, 1), c.repeat(1, monte_carlo_count, 1))
                 inputs = generated[:, -1].unsqueeze(1)
                 current_generated = generated
-                for i in range(remaining):
+                for i in range(remaining): 
                     _, hidden = self.lstm(inputs, hidden)
                     outputs = self.output_linear(hidden[0]).squeeze(0)
                     outputs = F.softmax(outputs, -1)
-                    predicted = outputs.multinomial(1)
+                    predicted = outputs.multinomial(1)      #multinomial ??? 对outputs取值1次？？？
                     # embed the next inputs, unsqueeze is required cause of shape (batch_size, 1, embedding_size)
                     inputs = self.embed.word_embeddings_from_indices(predicted.view(-1).cpu().data.numpy()).unsqueeze(
                         1).cuda()
                     current_generated = torch.cat([current_generated, inputs], dim=1)
-                reward = evaluator(image_features.repeat(monte_carlo_count, 1), current_generated)
+                reward = evaluator(image_features.repeat(monte_carlo_count, 1), current_generated)    # rewoard
                 reward = reward.view(batch_size, monte_carlo_count, -1).sum(1)
                 result += reward
                 result /= monte_carlo_count
