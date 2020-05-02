38eb33a076f83a1fb11b178868c5620fb9de3f89
Zora-zjj <54018191+Zora-zjj@users.noreply.github.com>
2020-05-02 15:32:10 +0800

    Update vgg_extractor.py

--- extractor/vgg_extractor.py
+++ extractor/vgg_extractor.py
@@ -25,23 +25,23 @@ class VggExtractor(BaseExtractor):
         for param in self.cnn.parameters():
             param.requires_grad = False
 
-    def extract(self, image):
+    def extract(self, image):      #image是图片路径
         if isinstance(image, str):
             image = cv2.imread(image)   #cv2.imread(filepath,flags)读入一副图片,filepath：要读入图片的完整路径,flags：读入图片的标志,默认彩色
         if isinstance(image, np.ndarray):
             image = cv2torch(image)   #？？？
-        image = self.trans(image)
+        image = self.trans(image)     #先transform
         image = image.float()
-        if len(image.size()) == 3:
-            image = image.unsqueeze(0)
+        if len(image.size()) == 3:        #[c,h,w]
+            image = image.unsqueeze(0)  #[b,c,h,w]
         if self.use_gpu:
             image = image.cuda()
         image = Variable(image)
-        temp = self.cnn.features(image)
-        return temp
+        temp = self.cnn.features(image)     # cnn.features() ????
+        return temp                         #提取到的特征
 
 
 if __name__ == '__main__':
     extractor = VggExtractor(use_gpu=True)
     image_path = FilePathManager.resolve("test_images/image_1.png")
-    print(extractor.extract(image_path))
+    print(extractor.extract(image_path))  
